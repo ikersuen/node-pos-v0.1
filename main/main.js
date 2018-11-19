a24e57967670c2b1b1bfc0404ba1aa1e76d6eaa6
Iker Suen <suenik@oocl.com>
2018-11-19 17:49:41 +0800

    In progress

--- main/main.js
+++ main/main.js
@@ -1,4 +1,42 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function printInventory(inputs) {
+
+var array_new = [];
+var neverExist = true;
+var output_string = '';
+
+for(var i = 0; i < inputs.length; i++){
+    for (var j = 0; j < array_new.length; j++){
+        if (array_new[j].Barcode == inputs[i]) {
+            array_new[i].count += 1;
+            neverExist = false;
+        }else{
+            neverExist = true
+        }
+    }
+    if(neverExist){
+        output.push({barcode:inputs[i].Barcode, name:inputs[i].Name, unit:inputs[i].Unit, price:inputs[i].Price, count:1});
+    }
+}
+
+function caclSum(array) {
+    var sum = 0;
+    for (var i = 0; i < array.length; i++) {
+        sum += (array[i].price);
+    }
+    return sum;
+};
+
+for(var k = 0; k < array_new; k++) {
+    output_string += 'Name: '+ array_new[k].name +', Quantity: 5 bottles, Unit price: ' + array_new[k].price +' (yuan), Subtotal: ' + array_new[k].price * array_new[k].sum +'(yuan)\n'
+}
+
+
+return
+    '***<store earning no money>Receipt ***\n' +
+    output_string +
+    '----------------------\n' +
+    'Total: '+ caclSum(array_new) +' (yuan)\n' +
+    '**********************\n';
+
+
 };
